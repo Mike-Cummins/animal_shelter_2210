e6d7616758fbb11e30e49f70544f4676eb6f7208
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-12 08:30:52 -0700

    Add passing code for add_pet

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,8 +1,12 @@
 class Shelter
   attr_reader :name, :capacity, :pets
-  def initialize(name, capacity)
-    @name = name
-    @capacity = capacity
-    @pets = []
-  end
+    def initialize(name, capacity)
+      @name = name
+      @capacity = capacity
+      @pets = []
+    end
+
+    def add_pet(pet)
+      @pets << pet
+    end
 end
